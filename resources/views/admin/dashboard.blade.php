2f02d60f9bea53103ae148003f09e70f8a1011be
bashir <bashirahmed.cse@gmail.com>
2023-02-10 22:02:07 +0600

    update

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.template')
+
+@section('mainContent')
+
+<h1>Hello Dashboard</h1>
+
+@endsection
---
+++ resources/views/admin/layouts/template.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html
+  lang="en"
+  class="light-style layout-menu-fixed"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="{{asset('dashboard/assets')}}"
+  data-template="vertical-menu-template-free"
+>
+  @include('admin.layouts.includes.header')
+
+  <body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <!-- Menu -->
+
+        @include('admin.layouts.includes.sidebar')
+        <!-- / Menu -->
+
+        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Navbar -->
+
+          @include('admin.layouts.includes.nav');
+
+          <!-- / Navbar -->
+
+          <!-- Content wrapper -->
+          <div class="content-wrapper">
+            @yield('mainContent')
+          </div>
+          <!-- Content wrapper -->
+        </div>
+        <!-- / Layout page -->
+      </div>
+
+      <!-- Overlay -->
+      <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
+
+    
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+
+    @include('admin.layouts.includes.scripts')
+ 
+</html>
